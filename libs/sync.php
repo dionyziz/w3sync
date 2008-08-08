281a1898a38e7abeb661489604cb9019abbf1a0e
Dionysis Zindros <dionyziz@gmail.com>
2008-08-08 02:06:09 +0000

    sync improvements

--- libs/sync.php
+++ libs/sync.php
@@ -16,11 +16,7 @@
         $revision = $match[ 'rev' ];
 
         exec( "diff /var/www/zino.gr/static/css/global-beta.css /var/www/zino.gr/static/css/global.css", $output, $ret );
-        var_dump( $output );
         exec( "diff /var/www/zino.gr/static/css/global-beta.js /var/www/zino.gr/static/css/global.js", $output, $ret );
-
-        var_dump( $output );
-        die();
 
         exec( "cat /var/www/zino.gr/static/css/global-beta.css > /var/www/zino.gr/static/css/global.css" );
         exec( "cat /var/www/zino.gr/static/js/global-beta.js > /var/www/zino.gr/static/js/global.js" );
