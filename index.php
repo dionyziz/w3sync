d2284c424625cefa25d2cd5110076a6cc6c1647d
Dionysis Zindros <dionyziz@gmail.com>
2008-08-11 21:12:28 +0000

    sync system

--- index.php
+++ index.php
@@ -65,6 +65,7 @@
     $lastSyncs = Log_GetLatest( 20 );
     ?><table><thead><tr><td>Revision</td><td>Developer</td><td>Type</td><td>Reason</td><td>Date</td><td>&nbsp;</td></tr></thead><tbody><?php
     $i = 1;
+    $j = 0;
     foreach ( $lastSyncs as $sync ) {
         ?><tr<?php
         if ( $i % 2 == 0 ) {
@@ -103,7 +104,7 @@
             echo date( "r", strtotime( $sync[ 'sync_created' ] ) );
         }
         ?></td><td><?php
-        if ( $sync[ 'sync_type' ] == 'sync' && $sync[ 'sync_revision' ] < $revision ) {
+        if ( $j != 0 && $sync[ 'sync_type' ] == 'sync' && $sync[ 'sync_revision' ] < $revision ) {
             ?><form action="sync.php" method="post">
                 <input type="hidden" name="do" value="sync" />
                 <input type="hidden" name="comment" value="Rolled back to revision <?php
@@ -112,11 +113,14 @@
                 <input type="hidden" name="revision" value="<?php
                 echo $sync[ 'sync_revision' ];
                 ?>" />
-                <input type="submit" value="Rollback" />
+                <a href="" onclick="if ( prompt( 'Are you sure you would like to rollback to revision <?php
+                echo $sync[ 'sync_revision' ];
+                ?>?' ) { this.parentNode.submit(); }return false">Rollback to here</a>
             </form><?php
         }
         ?></td></tr><?php
         ++$i;
+        ++$j;
     }
     ?></tbody></table>
     <h2>Sync locks</h2><?php
