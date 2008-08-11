1bc2f7e6603c2439bf1be86ac908e47282545c29
Dionysis Zindros <dionyziz@gmail.com>
2008-08-11 14:04:59 +0000

    rollback

--- libs/logging.php
+++ libs/logging.php
@@ -44,7 +44,7 @@
     function getLastSyncs( $limit = 20 ) {
         $limit = ( int )$limit;
         $sql = "SELECT
-                    `sync`.*, previoussync.sync_rev>`sync`.sync_rev AS rollback
+                    `sync`.*, previoussync.sync_rev>`sync`.sync_rev AS rollback, `users`.*
                 FROM
                     `sync` LEFT JOIN `users`
                         ON `sync_userid` = `user_id`
