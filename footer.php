44bd1c1e50d11751064141178e796372f0c44b04
Dionysis Zindros <dionyziz@gmail.com>
2008-08-11 23:28:03 +0000

    sync system

--- footer.php
+++ footer.php
@@ -1,6 +1,9 @@
 <?php
     echo ob_get_clean();
 ?>
+            <div class="copy">
+                &copy; 2008 Kamibu &#8212; We make cool stuff
+            </div>
         </div>
     </body>
 </html>
