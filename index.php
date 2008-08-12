f8e0568f8bd368dce02c3a1b031ee5e58246819c
Dionysis Zindros <dionyziz@gmail.com>
2008-08-12 02:09:15 +0000

    sync system

--- index.php
+++ index.php
@@ -141,7 +141,7 @@
     <h2>Sync locks</h2><?php
     if ( !count( $locks ) ) {
         ?>No active sync locks are currently placed.<br />
-        <small>Use sync locks to force no syncs for a limited time.</small><?php
+        <small>Use sync locks to enforce no syncs for a limited time.</small><?php
     }
     else {
         ?><table><thead><tr><td>Developer</td><td>Reason</td><td>Date</td><td>&nbsp;</td></tr></thead><tbody><?php
