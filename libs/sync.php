4bd2b5a53e920bb42b997392d183f76549d409ef
Christian Herrmann <happydoener@gmail.com>
2009-04-08 22:51:01 +0000

    Sync static on gaia

--- libs/sync.php
+++ libs/sync.php
@@ -42,10 +42,12 @@
     }
     
     function Sync_Static( $revision, $username, $comment ) {
-        exec( "sudo -u syncer svn up /var/www/zino.gr/static/css --revision " . $revision, $output, $ret );
+        /*exec( "sudo -u syncer svn up /var/www/zino.gr/static/css --revision " . $revision, $output, $ret );
         exec( "sudo -u syncer svn up /var/www/zino.gr/static/js --revision " . $revision, $output, $ret );
         exec( SVN_ROOT . "etc/generate-static.php production /var/www/zino.gr/static/css|/srv/svn/cssmin > /var/www/zino.gr/static/css/global.css" );
-        exec( SVN_ROOT . "etc/generate-static.php production /var/www/zino.gr/static/js|/srv/svn/jsmin > /var/www/zino.gr/static/js/global.js" );
+        exec( SVN_ROOT . "etc/generate-static.php production /var/www/zino.gr/static/js|/srv/svn/jsmin > /var/www/zino.gr/static/js/global.js" );*/
+		
+		exec( "wget -O - http://gaia.kamibu.com/sync/syncstatic.php?revision=" . $revision, $output, $ret );
 
         $data = implode( "\n", $output );
         $data .= "\nGenerated global.css and global.js revision " . $revision . ".\n";
