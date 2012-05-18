4e929af5644b4de890acf3503308df7da37c02fe
Dionysis Zindros <dionyziz@gmail.com>
2012-05-18 19:32:08 +0000

    Cleanup old credentials

--- libs/mysql.php
+++ libs/mysql.php
@@ -1,8 +1,8 @@
 <?php
-    $config[ 'mysql_user' ] = "backyard";
-    $config[ 'mysql_password' ] = "wdEaqYYP";
+    $config[ 'mysql_user' ] = "username";
+    $config[ 'mysql_password' ] = "password";
     $config[ 'mysql_host' ] = "127.0.0.1";
-    $config[ 'mysql_database' ] = "backyard";
+    $config[ 'mysql_database' ] = "database";
 
     if( !mysql_connect( $config[ 'mysql_host' ], $config[ 'mysql_user' ], $config[ 'mysql_password' ])) {
         die( "Mysql connection failed" );
